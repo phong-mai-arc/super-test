45446588a065cf21e89a8bbb054e0df5ed7614ad
Phong Mai <phong.mai@arctechnologies.io>
2025-01-22 14:57:23 +0800

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AppComponent as FMSComponent } from '../../fms/src/app/app.component';
+import { AppComponent as HISComponent } from '../../his/src/app/app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: 'FMS',
+    path: 'fms',
+    component: FMSComponent,
+  },
+  {
+    title: 'HIS',
+    path: 'his',
+    component: HISComponent,
+  }
+];
